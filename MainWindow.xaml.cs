902b4c1a54d5daf185caeb6e256ceed6f987e9a3
Dmytro <sagchik9@gmial.com>
2023-06-20 16:29:34 +0100

    fixed gameover menu

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -115,6 +115,8 @@ namespace Tetris
                 gameState.MoveBlockDown();
                 Draw(gameState);
             }
+
+            GameOverMenu.Visibility = Visibility.Visible;
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -153,9 +155,11 @@ namespace Tetris
             await GameLoop();
         }
 
-        private void PlayAgain_Click(object sender, RoutedEventArgs e)
+        private async void PlayAgain_Click(object sender, RoutedEventArgs e)
         {
-
+            gameState = new GameState();
+            GameOverMenu.Visibility = Visibility.Hidden;
+            await GameLoop();
         }
     }
 }
